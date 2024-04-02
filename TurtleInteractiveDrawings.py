c09c9a279befc9f88485ab7b8208269284396f69
Etem SARAC <esay35@gmail.com>
2024-04-02 15:11:10 +0300

    Turtle moves with 'space','Up'and'Down' -ANGLE

--- TurtleInteractiveDrawings.py
+++ TurtleInteractiveDrawings.py
@@ -9,7 +9,15 @@ turtle_instance = turtle.Turtle()
 def turtle_forward():
     turtle_instance.forward(100)
 
+def rotate_angle_right():
+    turtle_instance.right(100)
+
+def rotate_angle_left():
+    turtle_instance.left(100)
+
 drawing_board.listen()
 drawing_board.onkey(fun=turtle_forward,key='space')
+drawing_board.onkey(fun=rotate_angle_right,key='Up')
+drawing_board.onkey(fun=rotate_angle_left,key='Down')
 
 turtle.mainloop()
