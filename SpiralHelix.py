5d94d02b2a565806ad9dadabd5a6a60062e6d96a
Etem SARAC <esay35@gmail.com>
2024-04-02 14:44:22 +0300

    drawing Colorful Spriral Helix -fastest, new colors

--- SpiralHelix.py
+++ SpiralHelix.py
@@ -2,14 +2,17 @@ import turtle
 
 turtle_screen = turtle.Screen()
 turtle_screen = turtle.bgcolor('Light green')
-
+turtle.speed(0) #fastest=0
 
 turtle_instance = turtle.Turtle()
 turtle_instance.color('blue')
+turtle_colors = ['red', 'blue', 'orange', 'yellow', 'purple', 'green']
 
-for i in range(10):
+for i in range(5):
+    turtle_instance.color(turtle_colors [i % 6])
+    turtle_instance.pencolor(turtle_colors [i % 3])
     turtle_instance.circle(10 * i)
     turtle_instance.circle(-10 * i)
 
-
-turtle.done()
+turtle.mainloop()
+#turtle.done()
