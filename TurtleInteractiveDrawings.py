b9b73d245fde9ed9b5284afde46151063ac100a7
Etem SARAC <esay35@gmail.com>
2024-04-02 15:25:50 +0300

    What if we don't want Turtle to draw for a while...

--- TurtleInteractiveDrawings.py
+++ TurtleInteractiveDrawings.py
@@ -23,11 +23,20 @@ def clear_screen():
 def turtle_instance_home():
     turtle_instance.home()
 
+# What if we don't want Turtle to draw while coming back home or like this...
+def turtle_pen_up():
+    turtle_instance.penup()
+
+def turtle_pen_down():
+    turtle_instance.pendown()
+
 drawing_board.listen()
 drawing_board.onkey(fun=turtle_forward,key='space')
 drawing_board.onkey(fun=rotate_angle_right,key='Up')
 drawing_board.onkey(fun=rotate_angle_left,key='Down')
 drawing_board.onkey(fun=clear_screen,key='c')
 drawing_board.onkey(fun=turtle_instance_home,key='h')
+drawing_board.onkey(fun=turtle_pen_down,key='d')
+drawing_board.onkey(fun=turtle_pen_up,key='u')
 
 turtle.mainloop()
